8172348c80b16ab001206c3a4cbdd9f8bd70bd05
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-10-10 18:12:31 +0300

    finalize on for loop

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
-            System.out.println("I love dogs");
-        }
-        for (int i=0; i< 10; i++){
-            System.out.println(i);
+
+        int[] grades = {98, 100, 83, 90, 93};
+        for (int i =0; i < grades.length; i++){
+            System.out.println(grades[i]);
         }
 
     }
